fea4de1f1d81f93b675925a33bfadcfa7db5737b
red smile <85974401+R3dsm1le@users.noreply.github.com>
2026-06-27 22:21:06 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from api_connector import fetch_states
-import datetime
-import json 
-from data_parser import parse_data
-
-states = fetch_states()
-clean_states = parse_data(states)
-print(clean_states)
